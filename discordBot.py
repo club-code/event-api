ddac36d7c963d1f5d6a8faca12753395d15ce43f
Vincent Boisdon <43075363+boisdal@users.noreply.github.com>
2019-05-31 03:22:02 +0200

    add the previous bot.py file

---
+++ discordBot.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext import commands
+import random
+
+from token import TOKEN
+
+client = commands.Bot(command_prefix = '.')
+
+
+
+@client.event
+async def on_ready():
+    print('bot is ready')
+
+@client.command()
+async def ping():
+    await client.say("Wesh (eh non, ping c'est démodé)")
+
+@client.command(pass_context=True)
+async def createEvent(ctx, nomEvent, *options):
+    """
+    create an event with it's options
+    Synopsys : .createEvent nomEvent option1 option2 ...
+    """
+    event = Event(nomEvent)
+    for i in options:
+        if i[:5] == "date=":
+            event.addDate(i[5:])
+        else :
+            event.addOption(i)
+
+class Event:
+    def __init__(self, name):
+        self.name = name
+        self.date = ""
+        self.options = []
+        self.participants = []
+
+    def addDate(self, date):
+        self.date = date
+
+    def addOption(self, option):
+        self.options.append(option)
+
+client.run(TOKEN)
